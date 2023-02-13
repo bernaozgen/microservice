ffcaf565b368ba21db6f25f43a756469a0e93e7e
BERNA ÖZGEN <brnozgen@hotmail.com>
2023-02-13 04:12:56 +0300

    add maven repository

---
+++ inventoryService/src/test/java/com/kodlamaio/inventoryServer/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodlamaio.inventoryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
